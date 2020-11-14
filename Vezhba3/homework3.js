54588b5372e3bf0023350a5f704777c962864911
KobayagiNakamoto <63250177+KobayagiNakamoto@users.noreply.github.com>
2020-11-14 01:32:17 +0100

    first version of homework

---
+++ Vezhba3/homework3.js
@@ -0,0 +1,95 @@
+// 1. Write a JavaScript conditional statement to sort three numbers. Print the result.
+let a = 32;
+let b = -45;
+let c = 7;
+
+if (a > b && a > c) {
+  // nested if checks if b is bigger than c then order goes (a,b,c)
+  if (b > c) {
+    console.log(a + "," + b + "," + c);
+  } else {
+    // otherwise we print out (a, c, b)
+    console.log(a + "," + c + "," + b);
+  }
+} else if (b > a && b > c) {
+  // nested if checks if a is bigger than c then order goes (b, a, c)
+  if (a > c) {
+    console.log(b + "," + a + "," + c);
+  } else {
+    // otherwise we print out (b, c, a)
+    console.log(b + "," + c + "," + a);
+  }
+} else if (c > a && c > b) {
+  // nested if checks if a is bigger than b then order goes (c, a, b)
+  if (a > b) {
+    console.log(c + "," + a + "," + b);
+  } else {
+    // otherwise we print out (c, b, a)
+    console.log(c + "," + b + "," + a);
+  }
+  /* additional conditional statements that check if two of the numbers are equal,
+  to still preserve the ascending order, and finally to check if all the numbers are equal,
+  to print out that there is no order to be kept.
+  */
+} else if (a == b && a > c) {
+  console.log(a + "," + b + "," + c);
+} else if (b == c && b > a) {
+  console.log(b + "," + c + "," + a);
+} else {
+  console.log("where is the order if everybody is equal?");
+}
+
+// 2. Write a JavaScript conditional statement to find the largest of five numbers. Print the result.
+
+let d = -2;
+let e = 0;
+let f = -6;
+let g = 45;
+let h = 15;
+
+if (d > e && d > f && d > g && d > h) {
+  console.log(d);
+} else if (e > d && e > f && e > g && e > h) {
+  console.log(e);
+} else if (f > d && f > e && f > g && f > h) {
+  console.log(f);
+} else if (g > d && g > e && g > f && g > h) {
+  console.log(g);
+} else {
+  console.log(h);
+}
+
+// 3. Compute the average marks of the following students. Then, use the average to determine the corresponding grade.
+// Student_Name	Marks
+// David	        80
+// Vinoth	        77
+// Divya	        88
+// Ishitha	        95
+// Thomas	        68
+
+// Range	Grade
+// <60	    F
+// <70	    D
+// <80	    C
+// <90	    B
+// <100	    A
+
+let David = 80;
+let Vinoth = 77;
+let Divya = 88;
+let Ishitha = 95;
+let Thomas = 68;
+
+let averageMark = (David + Vinoth + Divya + Ishitha + Thomas) / 5;
+
+if (averageMark < 60) {
+  console.log(averageMark + " You've been graded with an F");
+} else if (averageMark < 70) {
+  console.log(averageMark + " You've been graded with a D");
+} else if (averageMark < 80) {
+  console.log(averageMark + " You've been graded with a C");
+} else if (averageMark < 90) {
+  console.log(averageMark + " You've been graded with a B");
+} else {
+  console.log(averageMark + "You've been graded with an A");
+}
